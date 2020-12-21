1eef288e81c98af9ff638d12a89cb89dd39d159b
BBHancock <bai.szsy@qq.com>
2020-12-21 19:57:50 +0800

    LOAN001 - init the project

---
+++ src/test/kotlin/com/icefrozen/loan/LoanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.icefrozen.loan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
