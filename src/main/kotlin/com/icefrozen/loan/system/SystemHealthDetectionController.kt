1b034f73e27cc5818c958a3469dc2897354e5fbc
BBHancock <bai.szsy@qq.com>
2020-12-22 16:05:36 +0800

    LOAN001 - add first controller

---
+++ src/main/kotlin/com/icefrozen/loan/system/SystemHealthDetectionController.kt
@@ -0,0 +1,15 @@
+package com.icefrozen.loan.system
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class SystemHealthDetectionController {
+
+    @GetMapping("/active/status")
+    fun activeStatus() {
+        return
+    }
+}
